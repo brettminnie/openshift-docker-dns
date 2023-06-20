19b6a4313c7512e0d9e99089d96a077d52288faa
Brett Minnie <github@bdb-studios.co.uk>
2023-06-20 14:13:14 +0100

    Now buiding, missing RFC files

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,14 @@ FROM ${BUILD_IMAGE}
 ENV DATA_DIR="/config" \
     BIND_USER=named
 
-COPY container_resources/entrypoint.sh /usr/local/bin/
+COPY container_resources/entrypoint.sh /usr/bin/
 RUN apk update && \
-    apk add bind==9.18.14-r1 bind-tools==9.18.14-r1 --no-cache && \
+    apk add bind==9.18.14-r1 bind-tools==9.18.14-r1 bash==5.2.15-r5 which==2.21-r4 --no-cache && \
     rm -rf /var/cache/apk/* && \
     mkdir /config && \
-    chmod +x /usr/local/bin/entrypoint.sh
-
+    chmod +x /usr/bin/entrypoint.sh
 VOLUME ${DATA_DIR}
 COPY container_resources/config ${DATA_DIR}/
-
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+ENTRYPOINT ["/usr/bin/entrypoint.sh"]
 
 CMD ["/usr/sbin/named", "-4", "-g", "-p", "1053", "-c", "/config/named.conf"]
--- container_resources/entrypoint.sh
+++ container_resources/entrypoint.sh
@@ -8,18 +8,18 @@ create_bind_data_dir() {
 
   # populate default bind configuration if it does not exist
   if [ ! -d ${BIND_DATA_DIR}/etc ]; then
-    mv /etc/named ${BIND_DATA_DIR}/etc
+    mv /etc/bind ${BIND_DATA_DIR}/etc
   fi
-  rm -rf /etc/named
-  ln -sf ${BIND_DATA_DIR}/etc /etc/named
+  rm -rf /etc/bind
+  ln -sf ${BIND_DATA_DIR}/etc /etc/bind
   chmod -R 0775 ${BIND_DATA_DIR}
   chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
 }
 
 create_pid_dir() {
-  mkdir -p /var/run/named
-  chmod 0775 /var/run/named
-  chown root:${BIND_USER} /var/run/named
+  mkdir -p /var/run/bind
+  chmod 0775 /var/run/bind
+  chown root:${BIND_USER} /var/run/bind
 }
 
 create_bind_cache_dir() {
@@ -38,18 +38,18 @@ create_bind_data_dir
 create_bind_cache_dir
 create_alias_create_key
 
-# allow arguments to be passed to named
+# allow arguments to be passed to bind
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$*"
   set --
-elif [[ ${1} == named || ${1} == "$(command -v named)" ]]; then
+elif [[ ${1} == bind || ${1} == "$(command -v bind)" ]]; then
   EXTRA_ARGS="${*:2}"
   set --
 fi
 
-# default behaviour is to launch named
+# default behaviour is to launch bind
 if [[ -z ${1} ]]; then
-  echo "Starting named..."
+  echo "Starting bind..."
   exec $(which named) -u ${BIND_USER} -g ${EXTRA_ARGS}
 else
   exec "$@"
